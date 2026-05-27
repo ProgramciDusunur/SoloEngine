de6178fd5816a6905412c10535faf6cc9af2f5f8
Eren Araz <lerenarazl@gmail.com>
2026-05-28 01:58:16 +0300

    add Negative Extensions bench: 130173

--- search.cpp
+++ search.cpp
@@ -483,6 +483,11 @@ int16_t negamax(Board& board, int depth, int16_t alpha, int16_t beta, int ply, S
             else if (singularBeta >= beta) {
                 return singularBeta;
             }
+
+            // Negative Extensions
+            else if (ttEntry.score >= beta) {
+                extension = -1;
+            }
         }
 
 
